7d556662804ae2277aa4ebd90a664d2cc8b67fa3
Igor Kovacenko <i.kvacenko@rainbowsoft.ru>
2020-10-14 18:00:10 +0400

    Исправлена логика model.js

--- public/js/helpers/model.js
+++ public/js/helpers/model.js
@@ -6,33 +6,37 @@ export default class Model {
             let xhr = new XMLHttpRequest();
             xhr.open('GET', url, false);
             xhr.send()
-            // проверка статуса HTTP запроса
-            if (xhr.status != 200) {
-                webix.message(xhr.status + ': ' + xhr.statusText, 'error');
-                reject()
-            } else {
-                // валидация статуса ответа сервера
-                if (!xhr.responseText.status) {
-                    webix.message('Не удалось совершить запрос', 'error');
-                    console.error(`GET xhr.responseText.status is ${xhr.responseText.status}`);
+            xhr.onload = () => {
+                // проверка статуса HTTP запроса
+                if (xhr.status != 200) {
+                    webix.message(xhr.status + ': ' + xhr.statusText, 'error');
                     reject()
-                }
+                } else {
+                    console.log(xhr.responseText)
 
-                // проверка статуса ответа сервера
-                switch (xhr.responseText.status) {
-                    case RESULT_STATE.SUCCESS: // положительный результат запроса
-                        resolve(xhr.responseText.data);
-                        return;
-                    case RESULT_STATE.FAILED: // отрицательный результат запроса
+                    // валидация статуса ответа сервера
+                    if (!xhr.responseText.status) {
                         webix.message('Не удалось совершить запрос', 'error');
-                        console.error(`GET ${xhr.responseText.error}`);
-                        reject();
-                        return;
-                    default: // ошибка при получении результата запроса
-                        webix.message('Не удалось совершить запрос', 'error');
-                        console.error(`GET Статус ответа сервера не соответствует ожидаемым значениям, xhr.responseText.status is ${xhr.responseText.status}`);
-                        reject();
-                        return;
+                        console.error(`GET xhr.responseText.status is ${xhr.responseText.status}`);
+                        reject()
+                    }
+
+                    // проверка статуса ответа сервера
+                    switch (xhr.responseText.status) {
+                        case RESULT_STATE.SUCCESS: // положительный результат запроса
+                            resolve(xhr.responseText.data);
+                            return;
+                        case RESULT_STATE.FAILED: // отрицательный результат запроса
+                            webix.message('Не удалось совершить запрос', 'error');
+                            console.error(`GET ${xhr.responseText.error}`);
+                            reject();
+                            return;
+                        default: // ошибка при получении результата запроса
+                            webix.message('Не удалось совершить запрос', 'error');
+                            console.error(`GET Статус ответа сервера не соответствует ожидаемым значениям, xhr.responseText.status is ${xhr.responseText.status}`);
+                            reject();
+                            return;
+                    }
                 }
             }
         })
@@ -44,33 +48,35 @@ export default class Model {
             let xhr = new XMLHttpRequest();
             xhr.open('POST', url, false);
             xhr.send(params)
-            // проверка статуса HTTP запроса
-            if (xhr.status != 200) {
-                webix.message(xhr.status + ': ' + xhr.statusText, 'error');
-                reject()
-            } else {
-                // валидация статуса ответа сервера
-                if (!xhr.responseText.status) {
-                    webix.message('Не удалось совершить запрос', 'error');
-                    console.error(`GET xhr.responseText.status is ${xhr.responseText.status}`);
+            xhr.onload = () => {
+                // проверка статуса HTTP запроса
+                if (xhr.status != 200) {
+                    webix.message(xhr.status + ': ' + xhr.statusText, 'error');
                     reject()
-                }
+                } else {
+                    // валидация статуса ответа сервера
+                    if (!xhr.responseText.status) {
+                        webix.message('Не удалось совершить запрос', 'error');
+                        console.error(`GET xhr.responseText.status is ${xhr.responseText.status}`);
+                        reject()
+                    }
 
-                // проверка статуса ответа сервера
-                switch (xhr.responseText.status) {
-                    case RESULT_STATE.SUCCESS: // положительный результат запроса
-                        resolve(xhr.responseText.data);
-                        return;
-                    case RESULT_STATE.FAILED: // отрицательный результат запроса
-                        webix.message('Не удалось совершить запрос', 'error');
-                        console.error(`GET ${xhr.responseText.error}`);
-                        reject();
-                        return;
-                    default: // ошибка при получении результата запроса
-                        webix.message('Не удалось совершить запрос', 'error');
-                        console.error(`GET Статус ответа сервера не соответствует ожидаемым значениям, xhr.responseText.status is ${xhr.responseText.status}`);
-                        reject();
-                        return;
+                    // проверка статуса ответа сервера
+                    switch (xhr.responseText.status) {
+                        case RESULT_STATE.SUCCESS: // положительный результат запроса
+                            resolve(xhr.responseText.data);
+                            return;
+                        case RESULT_STATE.FAILED: // отрицательный результат запроса
+                            webix.message('Не удалось совершить запрос', 'error');
+                            console.error(`GET ${xhr.responseText.error}`);
+                            reject();
+                            return;
+                        default: // ошибка при получении результата запроса
+                            webix.message('Не удалось совершить запрос', 'error');
+                            console.error(`GET Статус ответа сервера не соответствует ожидаемым значениям, xhr.responseText.status is ${xhr.responseText.status}`);
+                            reject();
+                            return;
+                    }
                 }
             }
         })
