41a2af5539ebe8678bfa0b77a153c7bde9f1fc86
Igor Kovacenko <i.kvacenko@rainbowsoft.ru>
2020-10-14 16:03:47 +0400

    Контроллер должностей

--- app/controllers/CPosition.go
+++ app/controllers/CPosition.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"database/sql"
+	"sample-project/app/helpers"
 	"sample-project/app/models/providers/position_provider"
 
 	"github.com/revel/revel"
@@ -14,10 +16,52 @@ type CPosition struct {
 
 // Before интерцептор контроллера CPosition
 func (c *CPosition) Before() (result revel.Result, rc CPosition) {
+	var (
+		connector helpers.IDBConnector // экземпляр коннектора, для получения экземпляра соединения с бд
+		db        *sql.DB              // экземпляр соединения с бд
+		err       error                // ошибка в ходе выполнения функции
+	)
+
+	// получение экземпляра соединения с бд
+	connector, err = helpers.GetConnector()
+	if err != nil {
+		revel.AppLog.Errorf("CPosition.Before : connector.GetDBConnection, %s\n", err)
+		return
+	}
+	db, err = connector.GetDBConnection()
+	if err != nil {
+		revel.AppLog.Errorf("CPosition.Before : connector.GetDBConnection, %s\n", err)
+		return
+	}
+
+	// инициализация провайдера
+	c.provider = new(position_provider.PPosition)
+	err = c.provider.Init(db)
+	if err != nil {
+		revel.AppLog.Errorf("CPosition.Before : c.provider.Init, %s\n", err)
+		return
+	}
+
 	return
+}
+
+// Destroy контроллера CPosition
+func (c *CPosition) Destroy() {
+	c.Controller.Destroy()
+
+	// удаление ссылки на провайдер
+	c.provider = nil
 }
 
 // GetAll получение всех должностей
 func (c *CPosition) GetAll() revel.Result {
-	return nil
+	// получение должностей
+	positions, err := c.provider.GetPositions()
+	if err != nil {
+		revel.AppLog.Errorf("CPosition.GetAll : c.provider.GetPositions, %s\n", err)
+		return c.RenderJSON(Failed(err.Error()))
+	}
+
+	// рендер положительного результата
+	return c.RenderJSON(Succes(positions))
 }
